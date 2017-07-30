6d0cbc079c18910785c49b5e41d9fdbe0479ab75
brockstar17 <brockdyer.git@gmail.com>
2017-07-30 15:31:08 -0400

    Base class for the mod started. Next step is adding in the framework such as the proxies.

---
+++ src/main/java/brockstar17/ArcaneAscension.java
@@ -0,0 +1,33 @@
+package brockstar17;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * The main class of the mod. <br>
+ * This is what forge will look for to inject the mod into minecraft.
+ * 
+ * @author Brockstar17
+ */
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class ArcaneAscension
+{
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+
+	}
+
+}
---
+++ src/main/java/brockstar17/Reference.java
@@ -0,0 +1,14 @@
+package brockstar17;
+
+/**
+ * A reference class for the mod, used for static values that do not change that
+ * are used throughout the mod.
+ * 
+ * @author Brockstar17
+ */
+public class Reference
+{
+	public static final String MODID = "arcaneascension";
+	public static final String NAME = "Arcane Ascension";
+	public static final String VERSION = "0.1";
+}
